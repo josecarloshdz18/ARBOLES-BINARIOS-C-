2b1801c10ac4e0f84f2524a1e45d5b5912a5cf98
Luisnerk <luis_calix@live.com>
2020-06-03 17:38:18 -0500

    Eliminar nodo

--- arboles.cpp
+++ arboles.cpp
@@ -63,6 +63,114 @@ void buscar(int dato, nodo* arbol, int cont) {
 
 }
 
+void cambio(nodo *arbol){
+	nodo *eliminar = NULL;
+	nodo *PNULL = NULL;
+	eliminar = arbol;
+	if (arbol->padre == NULL){	
+		if (eliminar->izquierda != NULL)
+			eliminar = eliminar->izquierda;
+		while (eliminar->derecha != NULL && arbol != NULL){
+			eliminar = eliminar->derecha;
+		}
+		arbol->dato = eliminar->dato;
+		if (eliminar->izquierda != NULL){
+			eliminar->padre->derecha = eliminar->izquierda;
+			eliminar->izquierda->padre = eliminar->padre;
+		}
+		else{
+			eliminar->padre->derecha = NULL;
+		}
+		
+	}
+	else{
+	if (eliminar->izquierda != NULL){
+			eliminar = eliminar->izquierda;
+		while (eliminar->derecha != NULL && arbol != NULL){
+			eliminar = eliminar->derecha;
+		}
+		arbol->dato = eliminar->dato;
+		if (eliminar->izquierda != NULL){
+			arbol->izquierda = eliminar->izquierda;
+			eliminar->izquierda->padre = arbol;
+		}
+		else{
+			eliminar->padre->izquierda = NULL;
+		}
+	}
+	else
+	if (eliminar->derecha != NULL && arbol->padre == NULL){
+		arbol = arbol->derecha;
+	}
+	else if (eliminar->derecha != NULL){
+		arbol = arbol->derecha;
+		eliminar->padre->derecha = arbol;
+		arbol->padre = eliminar->padre;
+	}
+	}
+	
+	delete eliminar;
+	eliminar = NULL;
+}
+
+// La función sirve para buscar el nodo a eliminar
+bool borrar(int dato, nodo *arbol){
+	bool encontrado = false;
+	if (arbol->dato == dato){
+		if (arbol->izquierda != NULL || arbol->derecha != NULL){
+			cambio(arbol);
+		}
+		else{
+			if (arbol->padre->derecha == arbol)
+				arbol->padre->derecha = NULL;
+			if (arbol->padre->izquierda == arbol)
+				arbol->padre->izquierda = NULL;
+			delete arbol;
+			arbol = NULL;	
+		}
+		return true;
+	}
+	else
+
+	// Se utiliza recursividad para avanzar a través del arbol mientras alguno de los hijos del nodo actual no sean nulos, y así buscar en todos los nodos
+	// recorriendo de arriba hacia abajo, y de izquierda a derecha (Pre-orden)
+	if (arbol->izquierda != NULL && arbol->derecha != NULL){	
+		if(dato < arbol->dato){
+			encontrado = borrar(dato, arbol->izquierda);
+		}
+		else if (dato > arbol->dato){
+			encontrado = borrar(dato, arbol->derecha);
+		}
+	}
+	return encontrado;
+}
+
+// Esta función sirve para que el usuario introduzca el dato que quiere borrar
+void borrar_op(){
+	int dato;
+	nodo *temp;
+	cout<<"¿Qué dato quiere eliminar?\n";
+	cin>>dato;
+
+	// En caso de que el dato sea igual al nodo raíz y no existan nodos en la parte izquierda del árbol, la parte derecha será el árbol nuevo, siendo el primer nodo derecho la raíz
+	if(arbol->dato == dato && arbol->izquierda == NULL){
+		temp = arbol;
+		arbol = arbol->derecha;
+		delete temp;
+		temp = NULL;
+		cout<<"\n\nEliminado con éxito\n";
+	}
+	else
+
+	// Caso contrario, se buscara el dato más adentro, y si se llega a encontrar, la función devolverá true
+	if (borrar(dato, arbol)){
+		cout<<"\n\nEliminado con éxito\n";
+	}
+	else{
+		cout<<"\n\n¡No se encontró el dato!\n";
+	}
+}
+
 int main() {
 	setlocale(LC_ALL, "spanish");
 	int op = 0;
